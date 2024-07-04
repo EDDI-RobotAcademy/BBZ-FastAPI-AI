8d31e32394f58cc358a21085b790bda1c708db29
이경민 <124436145+2kilometer@users.noreply.github.com>
2024-07-04 17:55:32 +0900

    init: [이경민] fastapi 초기 윤곽 설정 [BBZFPx]

---
+++ hotelbusterz_fastapi/app/main.py
@@ -0,0 +1,32 @@
+import aiomysql
+from fastapi import FastAPI
+
+from async_db.database import getMySqlPool
+from logistic_regression.controller.logistic_regression_controller import logisticRegressionRouter
+
+
+import warnings
+warnings.filterwarnings("ignore", category=aiomysql.Warning)
+
+async def lifespan(app: FastAPI):
+    # Startup
+    app.state.dbPool = await getMySqlPool()
+
+    yield
+
+    # Shutdown
+    app.state.dbPool.close()
+    await app.state.dbPool.wait_closed()
+
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+app.include_router(logisticRegressionRouter)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=33333)
